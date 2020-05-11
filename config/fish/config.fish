d986447065f3063b97a4abfd2ba91480d8a6f15a
jovi <zhuwei-ms@outlook.com>
2020-05-11 18:56:44 +0800

    add autojump and fnm

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,9 @@ set -g LANG en_US.UTF-8
 
 # Use vi-mode
 set -g fish_key_bindings fish_vi_key_bindings
+
+# fnm
+fnm env --multi | source
+
+# autojump
+[ -f /usr/local/share/autojump/autojump.fish ]
