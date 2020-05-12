46a0237a3225784ac0469bb94968338f28b2ebe8
jovi <zhuwei-ms@outlook.com>
2020-05-12 11:11:37 +0800

    update fish for fisher

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,5 +6,11 @@ set -g fish_key_bindings fish_vi_key_bindings
 # fnm
 fnm env --multi | source
 
-# autojump
-[ -f /usr/local/share/autojump/autojump.fish ]
+# To automate the installation process in a new system,
+# installing packages listed in your fishfile,
+# add the following code to your fish configuration file.
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
