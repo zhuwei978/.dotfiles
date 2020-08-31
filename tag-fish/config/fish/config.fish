7928ada6946f29cf892d6dca46da364bc54ff971
jovi <zhuwei-ms@outlook.com>
2020-09-01 00:24:34 +0800

    feat(fish): add bootstrap installtion

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -1,1 +1,8 @@
 fnm env --multi | source
+
+# To automate the installation process in a new system, installing packages in your fishfile
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
