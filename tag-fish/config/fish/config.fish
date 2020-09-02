6cd90bab5c5112af9d420a50efea9961261c2524
jovi <zhuwei-ms@outlook.com>
2020-09-02 15:10:55 +0800

    feat(fish): set fish abbrs and env variables

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -1,1 +1,20 @@
 fnm env --multi | source
+
+set -U LANG en_US.UTF-8
+set -U EDITOR nvim
+set -U HOMEBREW_NO_AUTO_UPDATE 1
+set -U HOMEBREW_BUNDLE_FILE $HOME/.Brewfile
+
+# Abbreviations
+if status --is-interactive
+    abbr -a -g lg lazygit
+    abbr -a -g ra ranger
+    abbr -a -g ngl npm global list
+    abbr -a -g y yarn
+    abbr -a -g ys yarn start
+    abbr -a -g yps yarn pstart
+    abbr -a -g ygl yarn global list
+    abbr -a -g yr yarn remove
+    abbr -a -g ya yarn add
+    abbr -a -g yad yarn add -D
+end
