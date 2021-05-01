f501c6aa07c885031cb55add7c56abc87b3ea86d
zhuw31 <meiyegg@gmail.com>
2021-05-01 20:39:44 +0800

    feat(nvim): remove plugin keymaps from keymappings.lua

--- tag-nvim/config/nvim/lua/keymappings.lua
+++ tag-nvim/config/nvim/lua/keymappings.lua
@@ -8,14 +8,3 @@ map('n', '<c-h>', '<c-w>h')
 map('n', '<c-j>', '<c-w>j')
 map('n', '<c-k>', '<c-w>k')
 map('n', '<c-l>', '<c-w>l')
-
--- akinsho/nvim-bufferline.lua
-map('n', '[b', '<cmd>BufferLineCycleNext<cr>', silent)
-map('n', ']b', '<cmd>BufferLineCyclePrev<cr>', silent)
-
--- nvim-telescope/telescope.nvim
-map('n', '<leader>ff', '<cmd>Telescope find_files prompt_prefix=📂<cr>')
-map('n', '<leader>fd', '<cmd>Telescope live_grep prompt_prefix=🔍<cr>')
-
--- kyazdani42/nvim-tree.lua
-map('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
