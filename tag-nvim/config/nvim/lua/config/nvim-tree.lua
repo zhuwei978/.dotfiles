022cf814283535570f55cc749184fcdf9c73720e
zhuw31 <meiyegg@gmail.com>
2021-07-19 22:20:24 +0800

    chore(nvim): remove stale settings

--- tag-nvim/config/nvim/lua/config/nvim-tree.lua
+++
@@ -1,7 +0,0 @@
-local utils = require'config.utils'
-local map = utils.map
-
-map('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
-
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_quit_on_open = 1
--- tag-nvim/config/nvim/lua/keymappings.lua
+++
@@ -1,9 +0,0 @@
-local utils = require'config.utils'
-local map = utils.map
-
--- Keybindings
--- Window movement
-map('n', '<c-h>', '<c-w>h')
-map('n', '<c-j>', '<c-w>j')
-map('n', '<c-k>', '<c-w>k')
-map('n', '<c-l>', '<c-w>l')
